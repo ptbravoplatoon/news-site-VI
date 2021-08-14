d27db11b3a5fdf818eb0cf6cd3bc61fa6b052616
jpeart03 <james.r.peart@gmail.com>
2021-08-14 09:04:18 -0500

    add login to UsersAPI module

--- src/api/UsersAPI.js
+++ src/api/UsersAPI.js
@@ -1,6 +1,15 @@
-const login = (credentialsObject) => {
-}
+const BASE_URL = "http://localhost:3001/api/users/";
 
-export default {
-  login: login
-}
+const login = async (credentialsObject) => {
+  const endpoint = `${BASE_URL}/login?include=user-${JSON.stringify(
+    credentialsObject
+  )}`;
+  const context = {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify(credentialsObject),
+  };
+  return fetch(endpoint, context);
+};
+
+export { login };
