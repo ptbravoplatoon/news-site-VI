25f34b274bb7a77976a50a116765544c7b7c1cd9
Jeffery Legg <jeffrey.d.legg@gmail.com>
2021-08-14 17:07:21 -0400

    update

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -2,43 +2,39 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, NavItem } from 'reactstrap';
 import navItems from '../../config/Sections.json';
+import UserContext from '../../contexts/UserContext';
 
-class AppNav extends Component {
-  render() {
-    return (
-      <Navbar color="light">
-        {navItems.map((navItem) =>
-          <NavItem>
-            <Link to={`/sections/${navItem.value}`} >
-              { navItem.label }
-            </Link>
-          </NavItem>
-        )}
+function AppNav() {
+  const userContext = React.useContext(UserContext);
+
+  return (
+    <Navbar color="light">
+      {navItems.map((navItem) =>
+        <NavItem>
+          <Link to={`/sections/${navItem.value}`} >
+            {navItem.label}
+          </Link>
+        </NavItem>
+      )}
+      {!userContext.user ? 
+        <NavItem>
+          <Link to={"/login"}>Login</Link>
+        </NavItem>
+      :
+        <div>
         <NavItem>
           <Link to="/add-article">Add an Article</Link>
         </NavItem>
-      </Navbar>
-    )
-  }
+        <NavItem>
+          <Link to={"/logout"}>Logout</Link>
+        </NavItem>
+        </div>
+      }
+    </Navbar>
+  );
 }
-
 export default AppNav;
 
 
 // Functional solution:
-// function AppNav() {
-//   return (
-//     <Navbar color="light">
-//       {navItems.map((navItem) =>
-//         <NavItem>
-//           <Link to={`/sections/${navItem.value}`} >
-//             {navItem.label}
-//           </Link>
-//         </NavItem>
-//       )}
-//       <NavItem>
-//         <Link to="/add-article">Add an Article</Link>
-//       </NavItem>
-//     </Navbar>
-//   );
-// }
+
